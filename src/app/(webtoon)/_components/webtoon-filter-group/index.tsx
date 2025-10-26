18084e73e036d31b6ab15e6fa5a13dcf295b7ee0
yooss2006 <yooss2006@naver.com>
2025-10-26 16:06:52 +0900

    Style: 웹툰 필터 버튼 Speed Dial 형식으로 변경

--- src/app/(webtoon)/_components/webtoon-filter-group/index.tsx
+++ src/app/(webtoon)/_components/webtoon-filter-group/index.tsx
@@ -1,14 +1,24 @@
+import { cn } from '@/lib/utils';
+
 import { FilterType } from './constants';
 import { WebtoonFilter } from './webtoon-filter';
 
 export function WebtoonFilterGroup() {
   return (
-    <ul className="flex items-center justify-center gap-4">
-      {Object.values(FilterType).map((type: FilterType) => (
-        <li key={type}>
-          <WebtoonFilter type={type} />
-        </li>
-      ))}
-    </ul>
+    <div className="group fixed right-4 bottom-40 md:right-6 md:bottom-6">
+      <button className="text-foreground h-12 w-12 rounded-full bg-[#3d383c]">+</button>
+      <ul
+        className={cn(
+          'flex gap-5 opacity-0 transition-all group-hover:opacity-100 md:flex-col-reverse',
+          'absolute right-17 bottom-[50%] translate-y-[50%] md:right-0 md:bottom-16 md:translate-y-0'
+        )}
+      >
+        {Object.values(FilterType).map((type: FilterType) => (
+          <li key={type}>
+            <WebtoonFilter type={type} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
